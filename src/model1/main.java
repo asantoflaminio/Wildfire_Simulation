cc146fa1229864fb2fda843e48f3250cee4822c9
Bianca Ritorto <britorto@itba.edu.ar>
2019-12-06 19:26:17 -0300

    Change capital letter M of class

--- src/model1/main.java
+++ src/model1/main.java
@@ -2,7 +2,7 @@ package model1;
 
 import java.io.IOException;
 
-public class Main {
+public class main {
     public static void main(String[] args) throws IOException {
         SimulationImpl simulation = new SimulationImpl(1,0.1, "terrains/elevTerrain.txt",450,200, "generated_files/");
         simulation.runSimulation();
