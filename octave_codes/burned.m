be339ccb41da76d91bf8b98c94fbe923875512ad
Alejandro <asantoflaminio@itba.edu.ar>
2019-12-13 17:58:19 -0300

    burned.m updated

--- octave_codes/burned.m
+++ octave_codes/burned.m
@@ -1,16 +1,17 @@
-input1 = dlmread('BurnedCells4.txt', '');
+input1 = dlmread('BurnedCells2.txt', '');
 input1([1,2],:) = [];
 x = input1(:,1);
 y = input1(:,2); %fraccion de particulas
 plot(x,y, "marker", 'o',"linestyle", "-", "color", "m", "linewidth", 1);
 hold on;
-xo = x(end/2 + 1 + 1/2); %valor del medio
+
+xo = x(end/2 + 1.5); %valor del medio
 
 ending = x(end);
 t = x;
 finalindex = find(y == max(y));
 finalvalue = y(finalindex);
-k = (0.15:0.0001:0.22);
+k = (0.1:0.01:0.5);
 
 for i=1:length(k)
   for j = 1:length(t) 
@@ -30,20 +31,21 @@ error = 0;
 for i = 1:length(k)
   c = f(i,:);
   for j = 1:length(t)
-   error += (y(j) - c(j))^2;
+   error += (c(j) - y(j))^2;
    %error
   endfor
-  e(i) = error;
+  e(i) = error/length(t);
   error = 0;
 endfor
 
-length(e)
+
 for i = 1:length(k)
   index = find(e == min(e));
   plot(x,f(index,:),'b-')
 hold on
 endfor
-k(index)
+valorK = k(index)
+errorTotal = min(e)
 
 set (gca, "xgrid", "on");
 set (gca, "ygrid", "on");
