65084203a89888d67cf73c44dd4e7ac764218d81
Alejandro <asantoflaminio@itba.edu.ar>
2019-12-14 01:28:42 -0300

    detail

--- src/model1/main.java
+++ src/model1/main.java
@@ -4,11 +4,10 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        SimulationImpl simulation = new SimulationImpl(false, 1,0.1, "terrains/simpleSquare.txt",249,249, "generated_files/");
-        simulation.runSimulation();
+        SimulationImpl simulation = new SimulationImpl(false, 1,0.1, "terrains/slopeSquare2000.txt",249,249, "generated_files/");
 //        TerrainCreator.degradeTerrainElevation();
 //        TerrainCreator.simpleSquare();
-//        TerrainCreator.slopeSquare(500);
+//        TerrainCreator.slopeSquare(100);
 //        TerrainCreator.degradeTerrainDensity();
     }
 }
