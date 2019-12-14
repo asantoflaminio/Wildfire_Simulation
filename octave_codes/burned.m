4b0127290c6d79507860b68e96752ca07b417872
Constanza De Rienzo <cderienzo@itba.edu.ar>
2019-12-14 02:56:39 -0300

    update burned.m

--- octave_codes/burned.m
+++ octave_codes/burned.m
@@ -1,17 +1,22 @@
-input1 = dlmread('BurnedCells2.txt', '');
+input1 = dlmread('BurnedCells_0.4_2.txt', '');
 input1([1,2],:) = [];
 x = input1(:,1);
 y = input1(:,2); %fraccion de particulas
-plot(x,y, "marker", 'o',"linestyle", "-", "color", "m", "linewidth", 1);
-hold on;
+%plot(x,y, "marker", 'o',"linestyle", "-", "color", "m", "linewidth", 1);
+%hold on;
+if(mod(length(x),2) ==0)
+  num = 1;
+else
+  num = 1.5;
+endif
 
-xo = x(end/2 + 1.5); %valor del medio
+xo = x(end/2 + num); %valor del medio
 
 ending = x(end);
 t = x;
 finalindex = find(y == max(y));
 finalvalue = y(finalindex);
-k = (0.1:0.01:0.5);
+k = (0.15:0.0001:0.25);
 
 for i=1:length(k)
   for j = 1:length(t) 
@@ -38,18 +43,18 @@ for i = 1:length(k)
   error = 0;
 endfor
 
-
-for i = 1:length(k)
   index = find(e == min(e));
-  plot(x,f(index,:),'b-')
-hold on
-endfor
+%for i = 1:length(k)
+ % index = find(e == min(e));
+ % plot(x,f(index,:),'b-')
+%hold on
+%endfor
 valorK = k(index)
 errorTotal = min(e)
 
-set (gca, "xgrid", "on");
-set (gca, "ygrid", "on");
-xlabel ("Tiempo [s]", "fontsize", 20);
-ylabel("Celdas quemadas", "fontsize", 20);
-set(gca, 'FontSize', 20)
-hold on; 
+%set (gca, "xgrid", "on");
+%set (gca, "ygrid", "on");
+%xlabel ("Tiempo [s]", "fontsize", 20);
+%ylabel("Celdas quemadas", "fontsize", 20);
+%set(gca, 'FontSize', 20)
+%hold on; 
